4ec9fe558c038af90c3ae35299d789115aed2887
CCjiahao <13462260020@163.com>
2024-02-20 13:10:32 +0800

    SIMECK64_DIFFERENCE_FROM_0x0_0x1_PRECISION_14

--- src/hyperparameter.hpp
+++ src/hyperparameter.hpp
@@ -43,7 +43,7 @@
 **/
 // 在这里定义启用哪一个实验
 
-#define SIMON64_LINEAR_FROM_0x40000004_0x1_PRECISION_14
+#define SIMECK64_DIFFERENCE_FROM_0x1_0x2_PRECISION_14
 
 /*************************** SIMECK48 ***************************/
 #ifdef SIMECK48_DIFFERENCE_FROM_0x20000_0x470000_PRECISION_14
